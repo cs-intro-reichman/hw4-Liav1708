4145d1a23420f3fb8727c0ff73e3d10fba5d5414
Liav Sarfati <liavsarfati@h-MacBook-Pro-sl-Liav.local>
2024-12-01 23:23:37 +0200

    check

--- Primes.java
+++ Primes.java
@@ -1,5 +1,22 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int input = Integer.parseInt(args[0]);
+        for(int i = 2; i < input; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int check){
+        boolean result = true;
+        int counter = 2;
+        while(counter < check){
+            if(check % counter == 0){
+                result = false;
+            }
+            counter++;
+        }
+        return result;
     }
 }
